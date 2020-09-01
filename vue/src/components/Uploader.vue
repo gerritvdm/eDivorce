a7035da78cee93c50385dace27ed2f6ca1abe86f
Michael Olund <michaelo@openroad.ca>
2020-09-01 14:41:51 -0700

    Integrated Vue.js with Django

---
+++ vue/src/components/Uploader.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="uploader">
+    <p>Drag and Drop the PDF document or JPG pages here or click here to Browse for files</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Uploader'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.uploader {
+  padding: 30px; 
+  border: 1px dashed grey;
+}
+</style>
