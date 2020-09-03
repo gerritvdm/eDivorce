8edcf55a998b59ffa0f1891232b8fb65b4099871
Michael Olund <michaelo@openroad.ca>
2020-09-02 17:25:59 -0700

    Added the vue dropzone component

--- vue/src/components/Uploader.vue
+++ vue/src/components/Uploader.vue
@@ -1,19 +1,28 @@
 <template>
-  <div class="uploader">
-    <p>Drag and Drop the PDF document or JPG pages here or click here to Browse for files</p>
-  </div>
+  <vue-dropzone ref="myVueDropzone" id="dropzone" :options="dropzoneOptions"></vue-dropzone>
 </template>
 
 <script>
+import vue2Dropzone from 'vue2-dropzone'
+import 'vue2-dropzone/dist/vue2Dropzone.min.css'
 export default {
-  name: 'Uploader'
+  name: 'Uploader',
+  components: {
+    vueDropzone: vue2Dropzone
+  },
+  data: function () {
+    return {
+      dropzoneOptions: {
+          url: 'https://httpbin.org/post',
+          thumbnailWidth: 150,
+          maxFilesize: 0.5,
+          dictDefaultMessage: "Drag and Drop the PDF document or JPG pages here or click here to Browse for files."
+      }
+    }
+  }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-.uploader {
-  padding: 30px; 
-  border: 1px dashed grey;
-}
 </style>
