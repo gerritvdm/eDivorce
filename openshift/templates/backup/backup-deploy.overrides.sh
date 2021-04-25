f005dab310f0f8682a67dfc50c8c0514fc95c904
Wade Barnes <wade@neoterictech.ca>
2021-04-25 05:10:51 -0700

    Update backup configuration.
    Signed-off-by: Wade Barnes <wade@neoterictech.ca>

--- openshift/templates/backup/backup-deploy.overrides.sh
+++ openshift/templates/backup/backup-deploy.overrides.sh
@@ -1,29 +1,32 @@
+_includeFile=$(type -p overrides.inc)
+if [ ! -z ${_includeFile} ]; then
+  . ${_includeFile}
+else
+  _red='\033[0;31m'; _yellow='\033[1;33m'; _nc='\033[0m'; echo -e \\n"${_red}overrides.inc could not be found on the path.${_nc}\n${_yellow}Please ensure the openshift-developer-tools are installed on and registered on your path.${_nc}\n${_yellow}https://github.com/BCDevOps/openshift-developer-tools${_nc}"; exit 1;
+fi
+
 # ========================================================================
 # Special Deployment Parameters needed for the backup instance.
 # ------------------------------------------------------------------------
 # The generated config map is used to update the Backup configuration.
 # ========================================================================
-
 CONFIG_MAP_NAME=backup-conf
-SOURCE_FILE=./config/backup.conf
+SOURCE_FILE=$( dirname "$0" )/config/backup.conf
+
 OUTPUT_FORMAT=json
-OUTPUT_FILE=backup-conf-configmap_DeploymentConfig.json
+OUTPUT_FILE=${CONFIG_MAP_NAME}-configmap_DeploymentConfig.json
 
-generateConfigMap() {  
-  _config_map_name=${1}
-  _source_file=${2}
-  _output_format=${3}
-  _output_file=${4}
-  if [ -z "${_config_map_name}" ] || [ -z "${_source_file}" ] || [ -z "${_output_format}" ] || [ -z "${_output_file}" ]; then
-    echo -e \\n"generateConfigMap; Missing parameter!"\\n
-    exit 1
-  fi
-
-  oc create configmap ${_config_map_name} --from-file ${_source_file} --dry-run -o ${_output_format} > ${_output_file}
-}
-
+printStatusMsg "Generating ConfigMap; ${CONFIG_MAP_NAME} ..."
 generateConfigMap "${CONFIG_MAP_NAME}" "${SOURCE_FILE}" "${OUTPUT_FORMAT}" "${OUTPUT_FILE}"
 
-SPECIALDEPLOYPARMS=""
-echo ${SPECIALDEPLOYPARMS}
 
+if createOperation; then
+  # Get the webhook URL
+  readParameter "WEBHOOK_URL - Please provide the webhook endpoint URL.  If left blank, the webhook integration feature will be disabled:" WEBHOOK_URL "false" 
+else
+  printStatusMsg "Update operation detected ...\nSkipping the prompts for the WEBHOOK_URL secret ...\n"
+  writeParameter "WEBHOOK_URL" "prompt_skipped" "false"
+fi
+
+SPECIALDEPLOYPARMS="--param-file=${_overrideParamFile}"
+echo ${SPECIALDEPLOYPARMS}
