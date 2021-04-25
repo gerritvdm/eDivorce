768729906b227c0146c9efc2440024a85bf0687a
Wade Barnes <wade@neoterictech.ca>
2021-04-25 08:11:09 -0700

    Update tool settings
    Signed-off-by: Wade Barnes <wade@neoterictech.ca>

--- openshift/settings.sh
+++ openshift/settings.sh
@@ -2,20 +2,7 @@ export PROJECT_NAMESPACE=${PROJECT_NAMESPACE:-ed44ad}
 export GIT_URI=${GIT_URI:-"https://github.com/bcgov/eDivorce.git"}
 export GIT_REF=${GIT_REF:-"master"}
 
-# The project components
-# - They are all contained under the main OpenShift folder.
-export components="."
-
 # The templates that should not have their GIT referances(uri and ref) over-ridden
 # Templates NOT in this list will have they GIT referances over-ridden
 # with the values of GIT_URI and GIT_REF
 export -a skip_git_overrides="schema-spy-build.yaml s2i-nginx-build.yaml backup-build.yaml clamav-build.yaml"
-
-# The builds to be triggered after buildconfigs created (not auto-triggered)
-export builds=""
-
-# The images to be tagged after build
-export images="nginx-proxy weasyprint edivorce-django postgresql"
-
-# The routes for the project
-export routes="nginx-proxy"
